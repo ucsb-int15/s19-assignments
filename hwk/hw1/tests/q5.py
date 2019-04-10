7025025e97fc24da055db02939c8aea8c8afd60d
nbgitpuller <nbgitpuller@example.com>
2019-04-10 15:56:43 +0000

    Homework 1 assignment

---
+++ hwk/hw1/tests/q5.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q5',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> isinstance(candidate, '
+                                               'str)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
