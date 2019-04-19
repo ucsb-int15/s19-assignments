0b34077f06020c8ea4ac54b2a0166ce5282fbcdd
ABC <abc@wxyzs-imac.cs.ucsb.edu>
2019-04-19 12:48:53 -0700

    Added Lab03

---
+++ lab/lab03/tests/q6.py
@@ -0,0 +1,20 @@
+test = {   'name': 'q6',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': ">>> 'Hour' in calls.columns\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> set(calls["Hour"]) == '
+                                               'set(range(24))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> list(calls["Hour"][:5]) == '
+                                               '[22, 21, 20, 8, 13]\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
