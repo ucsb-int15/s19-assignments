59e00bb066ac33780f99fd22b3fe4672dd4d5ab8
nbgitpuller <nbgitpuller@example.com>
2019-04-22 20:28:37 +0000

    Homework 2

---
+++ hwk/hw2/tests/q1d.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q1d',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> shot_zone_range_df.shape\n'
+                                               '(5,)',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hwk/hw2/tests/q1f.py
@@ -0,0 +1,15 @@
+test = {   'name': 'q1f',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'FGpercent_table.index.values\n'
+                                               'array([1, 2, 3, 4, 5])',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> FGpercent_table.shape\n'
+                                               '(5, 15)',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
