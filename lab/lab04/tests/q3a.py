bf657ab77f166cd9e6a30011f41ef267834da0d4
Kinto <kinto@169-231-132-10.wireless.ucsb.edu>
2019-04-26 12:37:20 -0700

    Added lab04

---
+++ lab/lab04/tests/q3a.py
@@ -0,0 +1,20 @@
+test = {   'name': 'q3a',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> diabetes_2d.shape\n'
+                                               '(442, 2)',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> -0.10 < '
+                                               'np.sum(diabetes_2d[0]) < 0\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> np.isclose(0, '
+                                               'np.sum(diabetes_2d))\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab04/tests/q3c.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q3c',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> 0.50 < two_dim_variance < '
+                                               '0.60\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
