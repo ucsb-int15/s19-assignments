088b5b58f1bf77005cd567892bd2419d0c44dcb4
nbgitpuller <nbgitpuller@example.com>
2019-05-24 19:43:22 +0000

    Added lab08

---
+++ lab/lab08/tests/q4b.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q4b',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> diabetes_features2.shape\n'
+                                               '(442, 11)',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab08/tests/q4c.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q4c',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'np.isclose(model2.rsquared, '
+                                               '0.14672786550959094)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
